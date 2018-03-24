421de899599bfcecb5954b45d6e8fa7daf1d6b6c
jyostna <ju36@njit.edu>
2018-03-24 00:30:58 -0400

    7.  Created a controller, route, and view for Contact Page and added to main nav

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/contact', 'PagesController@contact')->name('contact');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
