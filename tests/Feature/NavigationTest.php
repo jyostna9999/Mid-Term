7169a84b862ce27d24bab3dffe4215df1ea3d6f4
jyostna <ju36@njit.edu>
2018-03-24 01:58:42 -0400

    Feature test 3 - About page returns a 200 status code

--- tests/Feature/NavigationTest.php
+++ tests/Feature/NavigationTest.php
@@ -25,4 +25,10 @@ class NavigationTest extends TestCase
 
         $response->assertStatus(200);
     }
+    public function testAboutPage()
+    {
+        $response = $this->get('/about');
+
+        $response->assertStatus(200);
+    }
 }
